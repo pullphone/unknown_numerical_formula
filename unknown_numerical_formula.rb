c437c9175f507b7c4b0a2c0a2cacfc52f1ed8cad
pull <mail@pull-phone.com>
2015-01-19 19:53:07 +0900

    refactoring

--- unknown_numerical_formula.rb
+++ unknown_numerical_formula.rb
@@ -1,58 +1,68 @@
 class UnknownNumericalFormura
-  def self.calc(input)
-    return self._reduce(self._parse(input))
-  end
+  @@operators = ['*', '+', '&', '|']
 
-  def self._parse(input)
-    parsed = []
-
-    numeric = ''
-    input.chars.each do |i|
-      if (i =~ /\A-?\d+(.\d+)?\Z/) != nil
-        numeric += i
-        next
-      end
-
-      parsed.push(numeric.to_i)
-      parsed.push(i)
-      numeric = ''
+  class << self
+    def calc(input)
+      return _reduce(self._parse(input))
     end
-
-    parsed.push(numeric.to_i)
-    return parsed
-  end
-
-  def self._expr(ope, a, b)
-    return eval("#{a}#{ope}#{b}")
-  end
-
-  def self._reduce(input)
-    input_size = input.size
-    if input_size == 1
-      return input[0]
+  
+    def _parse(input)
+      parsed_input = []
+      numeric_str = ''
+  
+      input.chars.each {|char| parsed_input, numeric_str = _parse_char(parsed_input, numeric_str, char)}
+      parsed_input.push(numeric_str.to_i)
+  
+      return parsed_input
     end
-
-    left = []
-    right = []
-
-    operators = ['*', '+', '&', '|']
-    operators.each do |ope|
-      ope_index = input.index(ope)
-      if ope_index != nil
-        ope_index.times do |i|
-          left.push(input[i])
-        end
-
-        for i in (ope_index + 1)..(input_size - 1)
-          right.push(input[i])
-        end
-
-        if left.size == 1 && right.size == 1
-          return self._expr(ope, left[0], right[0])
-        end
-
-        return self._expr(ope, self._reduce(left), self._reduce(right))
+  
+    def _parse_char(parsed_input, numeric_str, char)
+      if (char =~ /[0-9]/) == nil
+        parsed_input.push(numeric_str.to_i)
+        parsed_input.push(char)
+        numeric_str = ''
+      else
+        numeric_str += char
       end
+  
+      return parsed_input, numeric_str
+    end
+  
+    def _reduce(input)
+      input_size = input.size
+      if input_size == 1
+        return input[0]
+      end
+  
+      @@operators.each do |operator|
+        result = _operate(input, operator)
+        return result if result != false
+      end
+    end
+  
+    def _operate(input, operator)
+      ope_index = input.index(operator)
+      if ope_index == nil
+        return false
+      end
+  
+      left, right = _divide_by_index(input, ope_index)
+  
+      if left.size == 1 && right.size == 1
+        return _expr(operator, left[0], right[0])
+      end
+  
+      return _expr(operator, self._reduce(left), self._reduce(right))
+    end
+  
+    def _divide_by_index(input, index)
+      left = input.slice!(0..index)
+      left.pop
+      return left, input
+    end
+  
+    def _expr(operator, a, b)
+      return eval("#{a}#{operator}#{b}")
     end
   end
 end
